005c1b9e5fc5168423c5eb75f1b7648584df5299
prats-2311 <prateek.srivastava2311@gmail.com>
2023-08-07 20:15:18 +0530

    client/server side validation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require("path");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError");
+const catchAsync = require("./utils/catchAsync");
+const { campgroundSchema } = require("./schemas");
 app.use(methodOverride("_method"));
 main().catch((err) => console.log(err));
 async function main() {
@@ -17,44 +20,85 @@ app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
 app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
-app.get("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const foundCampground = await Campground.findById(id);
-  res.render("campgrounds/show", { foundCampground });
-});
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const foundCampground = await Campground.findById(id);
+    res.render("campgrounds/show", { foundCampground });
+  })
+);
 
-app.post("/campgrounds", async (req, res) => {
-  const newCampground = req.body;
-  await Campground.insertMany(newCampground);
-  res.redirect("/campgrounds");
-});
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const editCampground = await Campground.findById(id);
-  res.render("campgrounds/edit", { editCampground });
-});
+app.post(
+  "/campgrounds",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) {
+    //   throw new ExpressError("Invalid operation", 400);
+    // }
 
-app.patch("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const edit = req.body;
-  // console.log(edit);
-  await Campground.findByIdAndUpdate(id, edit);
-  res.redirect("/campgrounds");
+    const newCampground = req.body.campground;
+    await Campground.insertMany(newCampground);
+    res.redirect("/campgrounds");
+  })
+);
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const editCampground = await Campground.findById(id);
+    res.render("campgrounds/edit", { editCampground });
+  })
+);
+
+app.patch(
+  "/campgrounds/:id",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const edit = req.body.campground;
+    // console.log(edit);
+    await Campground.findByIdAndUpdate(id, edit);
+    res.redirect("/campgrounds");
+  })
+);
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not found", 404));
 });
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
+app.use((error, req, res, next) => {
+  const { status = 500 } = error;
+  if (!error.message) error.message = "Oh No, Something went wrong";
+  res.status(status).render("error", { error });
 });
 app.listen(3000, () => {
   console.log("Serving on port 3000");
